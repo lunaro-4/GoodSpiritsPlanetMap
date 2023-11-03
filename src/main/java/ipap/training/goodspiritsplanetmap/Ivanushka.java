8b3b3ecb5224adf094379296fd3d5f388881a060
lunaro-4 <pavelike@gmail.com>
2023-11-03 12:06:32 +0300

    123

---
+++ src/main/java/ipap/training/goodspiritsplanetmap/Ivanushka.java
@@ -0,0 +1,323 @@
+package ipap.training.goodspiritsplanetmap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+
+//https://acm.timus.ru/problem.aspx?space=1&num=1210
+
+
+public class Ivanushka {
+    public static void main(String[] args) throws IOException {
+//        System.out.println(Arrays.deepToString(readFromFile("INPUT.TXT")));
+        Layer[] nodeMap = readFromFile("INPUT2.TXT");
+//        System.out.println(Arrays.toString(nodeMap));
+
+//        Layer[] nodeMap = readFromConsole();
+
+        Solution s = new Solution();
+        findSolution1(nodeMap, nodeMap.length-1, 0,s,-1);
+        int [][] adjMatr = convert2(nodeMap);
+        System.out.println(s.minPath);
+
+        int[] costToNodes = findSolution2(adjMatr);
+        int minValue = Integer.MAX_VALUE;
+        for (int i =costToNodes.length-1; i >costToNodes.length- nodeMap[nodeMap.length-1].nodeList.length; i--) {
+            minValue = Math.min(minValue, costToNodes[i]);
+        }
+        System.out.println( minValue);
+
+
+    }
+
+
+    private static int findSolution1(Layer[] nodeMap, int layerIndex, int prevNodesCost, Solution s, int nodeIndex) {
+        if (layerIndex>=0){
+            if ( nodeIndex == -1) {
+                for (nodeIndex = nodeMap[layerIndex].nodeList.length - 1; nodeIndex >=0; nodeIndex--) {
+                    for (int j = 0; j < nodeMap[layerIndex].nodeList[nodeIndex].connectionList.size(); j++) {
+                        checkConnection(nodeMap,layerIndex,prevNodesCost,s,nodeIndex,j);
+                    }
+                }
+            } else{
+                for (int j = 0; j < nodeMap[layerIndex].nodeList[nodeIndex].connectionList.size(); j++) {
+                    checkConnection(nodeMap,layerIndex,prevNodesCost,s,nodeIndex,j);
+                }
+            }
+        }else {
+//            System.out.println("\t" + s.curPath + "  " + s.minPath);
+            if (s.curPath < s.minPath)
+                s.minPath = s.curPath;
+        }
+        return 0;
+    }
+
+
+    private static int[] findSolution2(int[][] adjMatr){
+        int[] costToNode = new int[adjMatr[0].length];
+        Arrays.fill(costToNode, 999999);
+        for (int i = 1; i < adjMatr.length; i++) {
+            for (int j = 1; j < adjMatr[i].length; j++) {
+                int minCost;
+                if (i==1)
+                    minCost = adjMatr[i][j];
+                else
+                    minCost = Math.min(costToNode[i]+adjMatr[i][j], costToNode[j]);
+
+                costToNode[j]=minCost;
+            }
+        }
+//        System.out.println(Arrays.toString(costToNode));
+        return costToNode;
+    }
+    public static HashMap[] getDict(Layer[] nodeMap) {
+        //  converts nodeMap to adjacency matrix
+
+        Layer[] newMap = new Layer[nodeMap.length + 1];
+        Node[] nList = new Node[1];
+        Layer l0 = new Layer(nList, 0);
+        newMap[0] = l0;
+        for (int i = 0; i < nodeMap.length; i++) {
+            newMap[i + 1] = nodeMap[i];
+        }
+
+        HashMap<String, Integer> nameToMatrIndex = new HashMap<>();
+        HashMap<Integer,String > matrIndexToName = new HashMap<>();
+        int counter = 0;
+        for (int i = 0; i < newMap.length; i++) {
+            for (int j = 0; j < newMap[i].nodeList.length; j++) {
+                String s = "" + (i) + '.' + (j + 1); // nodes named from 1
+                counter++;
+                nameToMatrIndex.put(s, counter);
+                matrIndexToName.put(counter,s);
+            }
+        }
+        return new HashMap[] {nameToMatrIndex,matrIndexToName};
+    }
+
+
+    public static int[][] convert2(Layer[] nodeMap){
+        //  converts nodeMap to adjacency matrix
+
+        Layer[] newMap = new Layer[nodeMap.length+1];
+        Node[] nList = new Node[1];
+        Layer l0 = new Layer(nList ,0);
+        newMap[0] = l0;
+        for (int i = 0; i < nodeMap.length; i++) {
+            newMap[i+1]=nodeMap[i];
+        }
+
+        int counter = 0;
+        for (int i = 0; i < nodeMap.length; i++) {
+            for (int j = 0; j < nodeMap[i].nodeList.length; j++) {
+                counter++;
+            }
+        }
+        counter+=1;
+        HashMap<String,Integer> nameToMatrIndex = new HashMap<>();
+        // For some reason, algorythm creates -1.1 node, that does not lead anywhere,
+        // but only in y-axis. So we add 1 to x-axis to compensate.
+        int[][] adjMatr = new int[counter][counter+1];
+        for (int i = 0; i < adjMatr.length; i++) {
+            Arrays.fill(adjMatr[i], 99999);
+        }
+
+        counter =0;
+        for (int i = 0; i < newMap.length; i++) {
+            for (int j = 0; j < newMap[i].nodeList.length; j++) {
+                    String s =""+(i)+'.'+(j+1); // nodes named from 1
+                    counter++;
+                    nameToMatrIndex.put(s, counter);
+            }
+        }
+        //
+        for (int i = 1; i < newMap.length; i++) {
+            for (int j = 0; j < newMap[i].nodeList.length; j++) {
+                for (int k = 0; k < newMap[i].nodeList[j].connectionList.size(); k++) {
+                    String thisNodeName =""+i+'.'+(j+1); // nodes named from 1
+                    String toNodeName =""+(i-1)+'.'+(newMap[i].nodeList[j].connectionList.get(k).prevNodeIndex);
+                    int connectionCost = newMap[i].nodeList[j].connectionList.get(k).cost;
+                    adjMatr[nameToMatrIndex.get(toNodeName)][nameToMatrIndex.get(thisNodeName)] = connectionCost;
+                }
+            }
+        }
+
+//        System.out.println(nameToMatrIndex);
+//        System.out.println(Arrays.deepToString(adjMatr));
+//        ChemAlert2.printIntMatrix(adjMatr);
+        return adjMatr;
+    }
+
+
+    private static void convert(Layer[] nodeMap){
+        // converts nodeMap, so connections  stored in nodes of previous layer
+        Node[] nArr = new Node[1];
+        Layer l = new Layer(nArr, 0);
+        Layer[] newMap = new Layer[nodeMap.length];
+        newMap[0] = l;
+        for (int i = 1; i <= nodeMap.length; i++) {
+            for (int j = 0; j < nodeMap[i].nodeList.length; j++) {
+                List<Connection>[] cListArr = new List[nodeMap[i-1].nodeList.length];
+                for (int k = 0; k < cListArr.length; k++) {
+                    List<Connection> cList = new ArrayList<>();
+                    cListArr[k] = cList;
+                }
+                for (int k = 0; k < nodeMap[i].nodeList[j].connectionList.size(); k++) {
+                   Connection c = new Connection(   nodeMap[i].nodeList[j].connectionList.get(k).prevNodeIndex,
+                                                    nodeMap[i].nodeList[j].connectionList.get(k).cost );
+                    for (int m = 0; m < nodeMap[i-1].nodeList.length; m++) {
+                        cListArr[m].add(c);
+                    }
+                    for (int m = 0; m < cListArr.length; m++) {
+                        Node n = new Node(cListArr[m], i);
+                        newMap[i-1].nodeList[m] = n;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nodeMap));
+
+    }
+
+
+    private static void checkConnection(Layer[] nodeMap, int layerIndex, int prevNodesCost, Solution s, int nodeIndex, int connectionIndex){
+        int snap = s.curPath;
+        s.curPath +=nodeMap[layerIndex].nodeList[nodeIndex].connectionList.get(connectionIndex).cost;
+//        System.out.println("\t"+s.curPath+"  "+nodeIndex);
+        findSolution1(nodeMap,layerIndex-1,prevNodesCost,s,nodeMap[layerIndex].nodeList[nodeIndex].connectionList.get(connectionIndex).prevNodeIndex-1 );
+        s.curPath = snap;
+
+    }
+
+
+    private static Layer[] readFromConsole()throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int layers = sc.nextInt();
+        Layer[] nodeMap = new Layer[layers];
+        for (int i = 0; i < layers; i++) {
+            int nodesInLayer = sc.nextInt();
+            Node[] nList = new Node[nodesInLayer];
+            for (int j = 0; j < nodesInLayer; j++) {
+                List<Connection> cList = new ArrayList<>();
+                int prevNodeIndex = sc.nextInt();
+                while (prevNodeIndex!=0) {
+                    int nodeCost = sc.nextInt();
+                    Connection c = new Connection(prevNodeIndex, nodeCost);
+                    cList.add(c);
+                    prevNodeIndex = sc.nextInt();
+                }
+                Node n = new Node(cList, i);
+                nList[j] = n;
+            }
+            Layer l = new Layer(nList, i);
+            nodeMap[i] = l;
+            try {
+                String dump = sc.next();
+            } catch(Exception e) {System.out.println("End of File");}
+        }
+        return nodeMap;
+    }
+    public static Layer[] readFromFile(String filename)throws IOException{
+        Scanner sc = new Scanner(new File(filename));
+        int layers = sc.nextInt();
+        Layer[] nodeMap = new Layer[layers];
+        for (int i = 0; i < layers; i++) {
+            int nodesInLayer = sc.nextInt();
+            Node[] nList = new Node[nodesInLayer];
+            for (int j = 0; j < nodesInLayer; j++) {
+                List<Connection> cList = new ArrayList<>();
+                int prevNodeIndex = sc.nextInt();
+                while (prevNodeIndex!=0) {
+                    int nodeCost = sc.nextInt();
+                    Connection c = new Connection(prevNodeIndex, nodeCost);
+                    cList.add(c);
+                    prevNodeIndex = sc.nextInt();
+                }
+                Node n = new Node(cList, i);
+                nList[j] = n;
+            }
+            Layer l = new Layer(nList, i);
+            nodeMap[i] = l;
+            try {
+                String dump = sc.next();
+            } catch(Exception e) {System.out.println("End of File");}
+        }
+        return nodeMap;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
+    int minPath = Integer.MAX_VALUE;
+    int curPath = 0;
+    ArrayList<Integer> solList =new ArrayList<>();
+    ArrayList<Integer> pathList =new ArrayList<>();
+
+}
+
+class Layer{
+    Node[] nodeList;
+    int index;
+
+    public Layer(Node[] ndeList, int index) {
+        this.nodeList = ndeList;
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        return "Layer{" +
+                "index=" + index +
+                "\nnodeList=" + Arrays.toString(nodeList) +
+                "}\n";
+    }
+}
+class Node{
+    List<Connection> connectionList;
+    int layerIndex;
+    @Override
+    public String toString() {
+        return "\n\tNode{" +
+                "connectionList=" + connectionList +
+                '}';
+    }
+
+    public Node(List<Connection> connectionList, int layerIndex) {
+        this.connectionList = connectionList;
+        this.layerIndex =layerIndex;
+
+    }
+}
+class Connection{
+    int prevNodeIndex;
+    int cost;
+
+    public Connection(int prevNodeIndex, int cost) {
+        this.prevNodeIndex = prevNodeIndex;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\t\tConnection{" +
+                "prevNodeIndex=" + prevNodeIndex +
+                ", cost=" + cost +
+                '}';
+    }
+}
--- src/main/java/ipap/training/goodspiritsplanetmap/MapController.java
+++ src/main/java/ipap/training/goodspiritsplanetmap/MapController.java
@@ -1,45 +1,118 @@
 package ipap.training.goodspiritsplanetmap;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MapController {
 
-    @FXML
-    private Button btn1;
+    private final int Y_PADDING = 120;
+    private final int X_PADDING = 120;
+    private final int BTN_SIDE = 40;
 
-    @FXML
-    private Button btn2;
-
-
-    @FXML
-    private VBox mainBox;
-
-    @FXML
-    private HBox subMainBox;
 
 
     @FXML
-    protected void initialize(){
+    private Label debugText;
+
+
+    @FXML
+    private TextField fileField;
+
+    @FXML
+    private Button readFromFile;
+
+    @FXML
+    private AnchorPane mainBox;
+
+    @FXML
+    private AnchorPane subPane;
+
+
+    private HashMap<String, Button> nameToButton = new HashMap<>();
+    private int maxNodesInLayer;
+
+
+    @FXML
+    protected void initialize() {
+    }
+
+    private void drawLayer(double yBase,double xBase, Layer layer){
+        for (int i =0; i< layer.nodeList.length;i++) {
+            Button b = new Button(""+(layer.index+1)+"."+(i+1));
+            b.setLayoutX(xBase + X_PADDING*i);
+            b.setLayoutY(yBase);
+            b.setMinSize(BTN_SIDE,BTN_SIDE);
+//            b.setPrefHeight(BTN_SIDE);
+            b.setId(b.getText());
+            subPane.getChildren().add(b);
+            nameToButton.put(b.getText(),b);
+        }
+
+    }
+
+    private void drawGraph(Layer[] nodeMap){
+        for (int i = 0; i < nodeMap.length; i++) {
+            drawLayer((nodeMap.length-1)*Y_PADDING-i*Y_PADDING,X_PADDING,nodeMap[i]);
+        }
+        Layer layer = new Layer(new Node[]{new Node(new ArrayList<>(),0)}, -1);
+        drawLayer(nodeMap.length*Y_PADDING,X_PADDING+ X_PADDING/2,layer);
+        subPane.layout();
+        HashMap<Integer, String> indexToName = Ivanushka.getDict(nodeMap)[1];
+        int[][] nodemap2 = Ivanushka.convert2(nodeMap);
+        for (int i = 0; i < nodemap2.length; i++) {
+            for (int j = 0; j < nodemap2[i].length; j++) {
+                if (nodemap2[i][j] != 99999) {
+                    subPane.getChildren().add(
+                            drawPath(nameToButton.get(indexToName.get(i)),nameToButton.get(indexToName.get(j))));
+                }
+
+            }
+        }
+
+    }
+
+    @FXML
+    void readFromFile(ActionEvent event) {
+        subPane.getChildren().clear();
+        try {
+            Layer[] nodeMap = Ivanushka.readFromFile(fileField.getText());
+            maxNodesInLayer= getMaxNodesInLayer(nodeMap);
+            drawGraph(nodeMap);
+        } catch (IOException e){
+            debugText.setText("FileNotFound");
+        }
+    }
+
+    private int getMaxNodesInLayer(Layer[] nodeMap){
+        int maxNodes=-1;
+        for (Layer l :
+                nodeMap) {
+                if(l.nodeList.length>maxNodes)
+                    maxNodes=l.nodeList.length;
+            }
+        return maxNodes;
+        }
+
+
+
+
+    private Path drawPath(Button b1, Button b2){
         Path p = new Path();
-        p.getElements().add(new MoveTo(0.0f,0.0f));
-        p.getElements().add(new LineTo(50.0f,0.0f));
-        Path p2 = new Path();
-        p2.getElements().add(new MoveTo(btn1.getLayoutX(), btn1.getLayoutY()));
-        p2.getElements().add(new LineTo(btn2.getLayoutX(), btn2.getLayoutY()));
-        System.out.println(btn1.getLayoutX() + "  "+ btn1.getLayoutY());
-        System.out.println(btn1.getScaleX() + "  "+ btn1.getScaleY());
-        System.out.println(btn1.getTranslateX() + "  "+ btn1.getTranslateY());
-        System.out.println(btn2.getLayoutX() + "  "+ btn2.getLayoutY());
-        System.out.println(btn2.getScaleX() + "  "+ btn2.getScaleY());
-        System.out.println(btn2.getTranslateX() + "  "+ btn2.getTranslateY());
-        subMainBox.getChildren().add(p);
-        mainBox.getChildren().add(p);
+        p.getElements().add(new MoveTo(b1.getLayoutX()+BTN_SIDE/2, b1.getLayoutY()));
+        p.getElements().add(new LineTo(b2.getLayoutX()+BTN_SIDE/2, b2.getLayoutY()+BTN_SIDE));
+        p.setId(b1.getId()+" "+b2.getId());
+        return p;
     }
 
 }
